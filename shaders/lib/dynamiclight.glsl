e319a47909bd31c375086df14a77d3dddead584b
Hiba <132037918+Hiba550@users.noreply.github.com>
2026-08-17 05:50:35 +0530

    Update dynamiclight.glsl

--- shaders/lib/dynamiclight.glsl
+++ shaders/lib/dynamiclight.glsl
@@ -2,7 +2,6 @@
 //quillphen
 //GNU Affero General Public License v3.0
 //https://www.gnu.org/licenses/agpl-3.0.en.html
-//lol if you read this you are cool
 //goto gbuffers_terrain.fsh
 
 #ifndef DYNAMIC_LIGHT_GLSL
